b34f9f72eede813840e8ef9ff7420c910a4194dd
Anshul Chawla <anshulankush@yahoo.com>
2013-02-11 12:09:22 -0700

    Ranking using Vector Space Model of Crawled Web Pages

--- src/edu/asu/irs13/Preprocessing.java
+++ src/edu/asu/irs13/Preprocessing.java
@@ -1,5 +1,4 @@
-/*
- * PreProcessing class to Pre-Compute MaxFrequency of each Document
+/* PreProcessing class to Pre-Compute MaxFrequency of each Document
  * and Pre-compute sum of Squares of all terms in a document in documentModulus 
  */
 
@@ -8,88 +7,151 @@ package edu.asu.irs13;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.index.TermDocs;
 import org.apache.lucene.index.TermEnum;
 import org.apache.lucene.store.FSDirectory;
 
 public class Preprocessing {
 	public static void main(String[] args) throws Exception
-	{	//Preprocessing Start time
+	{	
+		//Pre-processing Start time
 		long start=System.currentTimeMillis();
+
 		//Index Reader to retrieve complete index in "r".
 		IndexReader r = IndexReader.open(FSDirectory.open(new File("index")));
-		//Initialising HashMap for storing maximum frequency and document modulus from files.
-		HashMap<Integer,Integer> maxFrequency= new HashMap<Integer,Integer>();
-		HashMap<Integer,Double> documentModulus= new HashMap<Integer,Double>();
-		// retrieving all terms from index.
-		TermEnum t = r.terms();
-		//t.next() gives next term
-		while(t.next()){
-			TermDocs td = r.termDocs(t.term());
-			//gives next document of that term
-			while(td.next()){
-				/*
-				 * calculating if max frequency already has the document id "or" if present the wether max frequent
-				 *  of document is less than term frequency of new term.
-				 */
-				if((maxFrequency.containsKey(td.doc())==false) || (maxFrequency.get(td.doc())<td.freq())){
-					// if yes then replace max with new term frequency
-					maxFrequency.put(td.doc(), td.freq());
-				}
+
+		/****************Calculating Inverse Document Frequency of each term**************/
+
+		TermEnum t3 = r.terms();
+		HashMap<String,Double> idf= new HashMap<String, Double>();
+		while(t3.next())
+		{
+			String currentTerm=t3.term().text();
+			Term te = new Term("contents",currentTerm );
+			if(r.docFreq(te)!=0){
+				idf.put(currentTerm, Math.log((double)25054/(double)r.docFreq(te)));
 			}
 		}
-		// retrieving all terms from index.
+
+		/*
+		 * Iterating for copying every key, value pair of HashMap to write IDF.
+		 */
+		FileWriter idfTXT;
+		idfTXT = new FileWriter("idf.txt");
+
+		for(Entry<String, Double> idfIterator: idf.entrySet()){
+			idfTXT.write(idfIterator.getKey()+" "+idfIterator.getValue()+"\n");
+		}
+
+		//Close FileWriter to finalize the copy action.
+		idfTXT.close();
+		long end=System.currentTimeMillis();
+		System.out.println("Time Taken in pre-processing IDF \nof all terms + time for File write operation: "+(end-start)+" milliseconds");
+
+		/********************Calculating Document Modulus for TF  ************************************************************/
+
+		start=System.currentTimeMillis();
+		HashMap<Integer,Double> documentModulusTf= new HashMap<Integer,Double>();
+
+		// Retrieving all terms from index.
 		TermEnum t1 = r.terms();
-		//t.next() gives next term
+
+		//t1.next() gives next term
 		while(t1.next()){
 			TermDocs td1 = r.termDocs(t1.term());
-			//gives next document of that term
+
+			//Gives next document of that term
 			while(td1.next()){
+
 				/*
-				 * calculating  Document Modulus , if not present
+				 * Calculating  Document Modulus , if not present
 				 *  then initial it to 0 in double.
 				 */
-				if(documentModulus.containsKey(td1.doc())==false){
-					documentModulus.put(td1.doc(), (double) 0);
+				if(documentModulusTf.containsKey(td1.doc())==false){
+					documentModulusTf.put(td1.doc(), (double) 0);
 				}
-				/*document modulus contains square of (termDoc value)/(maxTerm Value) of that instance.
-				 * 
+				/*
+				 * Here we we append square of frequency of each term in document with the frequency square of other terms.
 				 */
-				documentModulus.put(td1.doc(), (documentModulus.get(td1.doc())+Math.pow(((double)(td1.freq()))/((double)maxFrequency.get(td1.doc())), 2)));
+				documentModulusTf.put(td1.doc(), (documentModulusTf.get(td1.doc())+Math.pow(((double)(td1.freq())), 2)));
 			}
 		}
-		/*
-		 * write the computer hashmap of max frequency to text file maxFrequency.txt
-		 * write the computer hashmap of document modulus to text file documentModulus.txt
-		 */
-		FileWriter maxFrequencyCSV;
-		FileWriter documentModulusCSV;
+		FileWriter documentModulusTfTXT;
+		documentModulusTfTXT= new FileWriter("documentModulusTf.txt");
 
-		maxFrequencyCSV = new FileWriter("maxFrequency.txt");
-		documentModulusCSV= new FileWriter("documentModulus.txt");
 		/*
-		 * Iterating for copying every key, value pair of hasmap.
+		 * Iterating for copying every key, value pair of HashMmap.
+		 * Here (1/(square root of document) is calculated and added to file.
 		 */
-		for(Map.Entry<Integer, Integer> maxEntry: maxFrequency.entrySet()){
-			maxFrequencyCSV.write(maxEntry.getKey()+" "+maxEntry.getValue()+"\n");
+		end=System.currentTimeMillis();
+		System.out.println("\n\nTime Taken in pre-processing document modulus \nwith TF only + time for File write operation: "+(end-start)+" milliseconds");
+
+		for(Entry<Integer, Double> modulusEntry: documentModulusTf.entrySet()){
+			documentModulusTfTXT.write(modulusEntry.getKey()+" "+(1/(Math.sqrt(modulusEntry.getValue())))+"\n");
 		}
-		/*
-		 * Iterating for copying every key, value pair of hasmap.
-		 */
-		for(Entry<Integer, Double> maxEntry: documentModulus.entrySet()){
-			documentModulusCSV.write(maxEntry.getKey()+" "+maxEntry.getValue()+"\n");
+
+		//Close FileWriter to finalize the copy action.
+		documentModulusTfTXT.close();
+
+		
+		/********************Calculating Document Modulus for IDF************************************************************/
+		TermEnum t = r.terms();
+		Term te = new Term("contents", "body");
+		
+		// You can also quickly find out the number of documents that have term t
+		System.out.println("Number of documents with the word 'brute' is: " + r.docFreq(te));
+		start=System.currentTimeMillis();
+		HashMap<Integer,Double> documentModulusIdf= new HashMap<Integer,Double>();
+
+		// Retrieving all terms from index.
+		TermEnum t11 = r.terms();
+
+		//t11.next() gives next term
+		while(t11.next()){
+			TermDocs td11 = r.termDocs(t11.term());
+
+			//Gives next document of that term
+			while(td11.next()){
+
+				/*
+				 * Calculating  Document Modulus , if not present
+				 *  then initial it to 0 in double.
+				 */
+				if(documentModulusIdf.containsKey(td11.doc())==false){
+					documentModulusIdf.put(td11.doc(), (double) 0);
+				}
+				/*
+				 * Here we we append square of frequency of each term * IDF of that term in document with the frequency square of other terms.
+				 */				
+				if(idf.get(t11.term().text()) != null){
+					documentModulusIdf.put(td11.doc(), (documentModulusIdf.get(td11.doc())+Math.pow(((double)(td11.freq()*(idf.get(t11.term().text())))), 2)));
+
+				}
+				else{
+					documentModulusIdf.put(td11.doc(), (documentModulusIdf.get(td11.doc())+Math.pow(((double)(td11.freq())), 2)));
+				}
+			}
 		}
-		//close filewriter to finalize the copy action.
-		maxFrequencyCSV.close();
-		documentModulusCSV.close();
-		//computing end time to check total preprocessing time.
-		long end=System.currentTimeMillis();
-		System.out.println("Time Taken in pre-processing: "+(end-start));
+		end=System.currentTimeMillis();
+		System.out.println("\n\nTime Taken in pre-processing document \nfor TF-IDF + time for File write operation: "+(end-start)+" milliseconds");
 
-	}
+		FileWriter documentModulusIdfTXT;
+		documentModulusIdfTXT= new FileWriter("documentModulusIdf.txt");
 
+		/*
+		 * Iterating for copying every key, value pair of HashMmap.
+		 * Here (1/(square root of document) is calculated and added to file.
+		 */
+		for(Entry<Integer, Double> modulusEntry: documentModulusIdf.entrySet()){
+			documentModulusIdfTXT.write(modulusEntry.getKey()+" "+(1/(Math.sqrt(modulusEntry.getValue())))+"\n");
+		}
+		//Close FileWriter to finalize the copy action.
+		documentModulusIdfTXT.close();
+
+		//Computing end time to check total pre-processing time.
+			}
 }
--- src/edu/asu/irs13/SearchFiles.java
+++ src/edu/asu/irs13/SearchFiles.java
@@ -1,155 +1,245 @@
 package edu.asu.irs13;
 
-import org.apache.lucene.index.*;
-import org.apache.lucene.store.*;
-//import org.apache.lucene.document.*;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermDocs;
+import org.apache.lucene.index.TermEnum;
+import org.apache.lucene.store.FSDirectory;
+
 public class SearchFiles {
 
 	public static void main(String args[]) throws Exception{
-		//Index Reader to retrieve complete index in "r".
+
 		IndexReader r = IndexReader.open(FSDirectory.open(new File("index")));
-// buffered reader to read from maxFrequency.txt and documentModulus.txt
-		BufferedReader readMaxFrequency;
-		readMaxFrequency = new BufferedReader (new FileReader ("maxFrequency.txt"));
-//
-		String readDataByRow = readMaxFrequency.readLine();
-		HashMap<Integer,Integer> maxFrequency=new HashMap<Integer,Integer>();
-		while (readDataByRow != null){
-			String[] dataArray = readDataByRow.split(" ");
-			//System.out.print(dataArray[0] + "\t"+dataArray[1]); 
+		String str1 = "";
+		long read1=System.currentTimeMillis();
 
+		/************* Reading Preprocessed IDF from idf.txt******************/
 
-			//contains max frequency of document that is read from file maxFrequency.txt
-			maxFrequency.put(Integer.parseInt(dataArray[0]),Integer.parseInt(dataArray[1]));
-			//System.out.println();
-			readDataByRow = readMaxFrequency.readLine(); // Read next line of data.
+		BufferedReader idfLog;
+		idfLog = new BufferedReader (new FileReader ("idf.txt"));
+		String dataRow2 = idfLog.readLine();
+
+		//Initializing HashMap to retrieve idfLogMap (This HashMap will contain IDF values which will be used in project).
+		HashMap<String,Double> idfLogMap=new HashMap<String,Double>();
+
+		//Entering key as document number and value as IDF after Log (25054/doc frequency of term).
+		while (dataRow2 != null){
+			String[] dataArray2 = dataRow2.split(" ");
+
+			//Contains terms and their IDF values.
+			idfLogMap.put(dataArray2[0],Double.parseDouble(dataArray2[1]));
+
+			// Read next line of data.
+			dataRow2 = idfLog.readLine(); 
 		}
-		readMaxFrequency.close();	
-		System.out.println("24870 "+maxFrequency.get(24870));
 
-		BufferedReader readModulus;
-		readModulus = new BufferedReader (new FileReader ("documentModulus.txt"));
-		String dataRow1 = readModulus.readLine();
-		HashMap<Integer,Double> documentModulus=new HashMap<Integer,Double>();
+		//Closing write operation to confirm read operation.
+		idfLog.close();
+
+		/**************** Reading from preprocessed document modulus of TF from documentModulusTf.txt***********/
+		BufferedReader readModulusTf;
+		readModulusTf = new BufferedReader (new FileReader ("documentModulusTf.txt"));
+		String dataRow1 = readModulusTf.readLine();
+
+		//Initializing HashMap to retrieve documentModulus(This has all the values of document modulus TF).
+		HashMap<Integer,Double> documentModulusTf=new HashMap<Integer,Double>();
+
+		//Entering key as document number and value as document modulus (1/|d|).
 		while (dataRow1 != null){
 			String[] dataArray1 = dataRow1.split(" ");
-			//System.out.print(dataArray1[0] + "\t"+dataArray1[1]); 
 
-			//contains square-root of document modulus in that is read from file documentModulus.txt
-			documentModulus.put(Integer.parseInt(dataArray1[0]),Math.sqrt(Double.parseDouble(dataArray1[1])));
-			//System.out.println();
-			dataRow1 = readModulus.readLine(); // Read next line of data.
+			//Contains square-root of document modulus in that is read from file documentModulus.txt
+			documentModulusTf.put(Integer.parseInt(dataArray1[0]),Double.parseDouble(dataArray1[1]));
+
+			// Read next line of data.
+			dataRow1 = readModulusTf.readLine(); 
 		}
-		readMaxFrequency.close();	
-		readModulus.close();
-		//System.out.println("24870 "+maxFrequency.get(24870));
-		//System.out.println("24870 "+documentModulus.get(24870));
 
+		//Closing write operation to confirm read operation.
+		readModulusTf.close();
+
+		/**************** Reading from preprocessed document modulus of IDF from documentModulusIdf.txt***********/
+		Term te = new Term("contents", "title");
+		
+		// You can also quickly find out the number of documents that have term t
+		System.out.println("Number of documents with the word 'brute' is: " + r.docFreq(te));
+		//System.out.println(idfLogMap.get("html"));
+		
+		BufferedReader readModulusIdf;
+		readModulusIdf = new BufferedReader (new FileReader ("documentModulusIdf.txt"));
+		String dataRow11 = readModulusIdf.readLine();
+
+		//Initializing HashMap to retrieve documentModulus.
+		HashMap<Integer,Double> documentModulusIdf=new HashMap<Integer,Double>();
+
+		//Entering key as document number and value as document modulus (1/|d|).
+		while (dataRow11 != null){
+			String[] dataArray1 = dataRow11.split(" ");
+
+			//Contains square-root of document modulus in that is read from file documentModulus.txt
+			documentModulusIdf.put(Integer.parseInt(dataArray1[0]),Double.parseDouble(dataArray1[1]));
+
+			// Read next line of data.
+			dataRow11 = readModulusIdf.readLine(); 
+		}
+		long read2=System.currentTimeMillis();
+		System.out.println("Time taken to read: "+(read2-read1)+"milliseconds ");
+		//Closing write operation to confirm read operation.
+		readModulusIdf.close();
+
+		//Snippet implemented from Sample Code given for the project.
 		Scanner sc = new Scanner(System.in);
 		String str = "";
-		long startQuery=System.currentTimeMillis();
 		System.out.print("query> ");
-		HashMap<String,Integer> termMap=new HashMap<String,Integer>();
-		//	ArrayList<String> array=new ArrayList<String>();
+
 		while(!(str = sc.nextLine()).equals("quit")){
+			str1="";
+
+			//User input choice of method between TF and TF-IDF technique.
+			Scanner sc1 = new Scanner(System.in);
+			System.out.print("Press 1 for TF and 2 for TF-IDF> ");
+			str1=sc1.nextLine();
+
+			// Start of computation time for query. 
+			long startQuery=System.currentTimeMillis();
+
+			/*********Counting occurance of a term in query*********/
+
+			//Initial HashMap namely termMap to store number of occurrences of each term from query.
+			HashMap<String,Integer> termMap=new HashMap<String,Integer>();
 			String[] terms = str.split("\\s+");
 			for(String word : terms){	
 
+				//entering value 1 for the first occurrence of term from query.
 				if(termMap.containsKey(word)==false){
-					//			array.add(word);
 					termMap.put(word, 1);
 				}
 				else{
+
+					//value of term in query increments after every subsequent occurrence.
 					termMap.put(word, (termMap.get(word)+1));
 				}
+			}
+			/********Calculating |q| *********/
+
+			//Calculating Square of term frequency of all term in query i.e. (t1)^2+(t2)^2....
+			double querySquare=0;
+			for(Map.Entry<String, Integer> querySquareIterator: termMap.entrySet()){
+				int eachQueryTermFrequency=querySquareIterator.getValue();
+
+				//query square without dividing by querymax i.e. max term frequency of a term in query corpus.
+				querySquare+=Math.pow(((double)eachQueryTermFrequency), 2);			
 
 			}
-			System.out.println("the"+termMap.get("the"));
-			System.out.println("g"+termMap.get("great"));
-			int max=0;
-			for(Map.Entry<String, Integer> maxEntry: termMap.entrySet()){
-				int eachQueryTermFrequency=maxEntry.getValue();
-				if(max<eachQueryTermFrequency){
-					max=eachQueryTermFrequency;
-				}
-			}
-			double querySquare=0;
-			for(Map.Entry<String, Integer> maxEntry: termMap.entrySet()){
-				int eachQueryTermFrequency=maxEntry.getValue();
-				querySquare+=Math.pow(((double)eachQueryTermFrequency)/((double)max), 2);			
-			}
-			System.out.println("max "+max);
-			double queryModulus=Math.sqrt(querySquare);
-			System.out.println("queryModulus"+queryModulus);
+
+			//Calculating 1/sq. root of querySquare to obtain 1/|q|. 
+			double queryModulus=(1/Math.sqrt(querySquare));
 			HashMap<Integer,Double> dotProductMap=new HashMap<Integer,Double>();
 
-			for(Map.Entry<String, Integer> maxEntry: termMap.entrySet()){
-				String wordTerm=maxEntry.getKey();
-				int wordFrequency=maxEntry.getValue();
+			for(Map.Entry<String, Integer> termFrequencyIterator: termMap.entrySet()){
+				String wordTerm=termFrequencyIterator.getKey();
+				int wordFrequency=termFrequencyIterator.getValue();
 				Term term = new Term("contents", wordTerm);
 				TermDocs tdocs = r.termDocs(term);
-				while(tdocs.next())
-				{
+				//for each document having a term ti.
+				while(tdocs.next()){
+					double dotProduct = 0;
 
-					double dotProduct=((double)wordFrequency/max)*((double)(tdocs.freq())/maxFrequency.get(tdocs.doc()));
+					/*
+					 * Calculating with only TF.
+					 */
+					if(str1.equals("1")){
+
+						//calculating without dividing by max frequency of term per document and max term frequency per query.(ti*di)
+						dotProduct=((double)wordFrequency)*((double)(tdocs.freq()));
+					}
+
+					/*
+					 * Calculating With TF-IDF
+					 */
+					else if(str1.equals("2")){
+						//Calculating without max term frequency from query and max term frequency from document.(t*d*idf)
+						dotProduct=((double)wordFrequency)*((double)(tdocs.freq()))*(idfLogMap.get(wordTerm));
+					}
+					else{
+						System.out.println("wrong choice");
+						System.exit(0);
+					}
 					if((dotProduct!=(double)0)){
 						if(dotProductMap.containsKey(tdocs.doc())==false){
-							System.out.println("Dot Product"+dotProduct);
-
 							dotProductMap.put(tdocs.doc(), (double)0);
 						}
+
+						/*
+						 * After all dot product(q*d) of one term is calculated for all documents then dot product
+						 *of next term is calculated for all documents and is appended to existing dot product(q *d) of
+						 *the previously calculated terms.
+						 */
 						dotProductMap.put(tdocs.doc(), (dotProductMap.get(tdocs.doc())+dotProduct));
 					}
 				}
 			}
+			//Initializing HashMap for calculating final TF similarity.
+			//dot product* query modulus*document modulus.
 			HashMap<Integer,Double> similarityMap=new HashMap<Integer,Double>();
+			for(Entry<Integer, Double> dotProductIterator: dotProductMap.entrySet()){
+				/*
+				 * In case of TF only
+				 * 
+				 */
 
-			for(Entry<Integer, Double> maxEntry: dotProductMap.entrySet()){
-				similarityMap.put(maxEntry.getKey(),((maxEntry.getValue())*(1/(queryModulus))*(1/(documentModulus.get(maxEntry.getKey())))));			
-			}
-			for(Entry<Integer, Double> maxEntry: similarityMap.entrySet()){
-				System.out.println("similarity of "+maxEntry.getKey()+" is "+maxEntry.getValue());
-			}
+				if(str1.equals("1")){
 
-			for(Entry<Integer, Double> maxEntry: sortIntegerMap(similarityMap).entrySet()){
-				System.out.println(" correct similarity of "+maxEntry.getKey()+" is "+maxEntry.getValue());
-			}
-
-			for(Entry<Integer, Double> maxEntry: sortIntegerMap(similarityMap).entrySet()){
-				String d_url = r.document(maxEntry.getKey()).getFieldable("path").stringValue().replace("%%", "/");
-				System.out.println("["+maxEntry.getKey()+"] ["+maxEntry.getValue()+"] " + d_url);
-				System.out.println("query> ");
-			}
-			long endQuery=System.currentTimeMillis();
-			System.out.println("Query Run Time: "+(endQuery-startQuery));
-			System.out.print("query> ");
-		}
-		long endProgram=System.currentTimeMillis();
-		System.out.println("Program Run Time: "+(endProgram-StartProgram));
-	}
-	public static LinkedHashMap<Integer, Double> sortIntegerMap(Map<Integer, Double> unsortedMap){
-		LinkedHashMap<Integer, Double> sortedMap=new LinkedHashMap<Integer, Double>();
-		LinkedList<Double> valueList= new LinkedList<Double>(unsortedMap.values());
-		Collections.sort(valueList,Collections.reverseOrder());
-		for(int i=0;i<valueList.size();i++){
-			for(Map.Entry<Integer, Double> map:unsortedMap.entrySet()){
-				if(map.getValue()==valueList.get(i)){
-					sortedMap.put(map.getKey(), map.getValue());
+					similarityMap.put(dotProductIterator.getKey(),(dotProductIterator.getValue()*(queryModulus)*(documentModulusTf.get(dotProductIterator.getKey()))));			
+				}
+				/*
+				 * In case of IDF only.
+				 */
+				else if(str1.equals("2")){
+					similarityMap.put(dotProductIterator.getKey(),(dotProductIterator.getValue()*(queryModulus)*(documentModulusIdf.get(dotProductIterator.getKey()))));			
 				}
 			}
+			read2=System.currentTimeMillis();
+			System.out.println("Time taken to compute result: "+(read2-startQuery)+"milliseconds ");
+			/*
+			 * Using list with map inside and sorting in descending order using comparator.
+			 */
+			List<Entry<Integer, Double>> entries = new ArrayList<Entry<Integer, Double>>(similarityMap.entrySet());
+			Collections.sort(entries, new Comparator<Entry<Integer, Double>>() {
+				public int compare(Entry<Integer, Double> e1, Entry<Integer, Double> e2) {
+					return e2.getValue().compareTo(e1.getValue()); // Sorts descending.
+				}
+			});
+
+			// Put entries back in an ordered map.
+			Map<Integer, Double> orderedMap = new LinkedHashMap<Integer, Double>();
+			for (Entry<Integer, Double> entry : entries) {
+				orderedMap.put(entry.getKey(), entry.getValue());
+			}
+			for(Entry<Integer, Double> sortIterator: orderedMap.entrySet()){
+				//Displaying sorted Documents with document number.
+				System.out.println("["+sortIterator.getKey()+"]");
+			}
+			long endQuery=System.currentTimeMillis();
+			System.out.println("Query Sort Time: "+(endQuery-read2)+" milliseconds");
+
+			System.out.println("Query Search Time: "+(endQuery-startQuery)+" milliseconds");
+			System.out.println();
+			System.out.print("Next query> ");
 		}
-		return sortedMap;
 	}
 }
